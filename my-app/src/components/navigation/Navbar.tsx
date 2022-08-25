e1082288f5d803fb6ab0700fa18d08d000651d52
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-08-25 08:39:26 -0300

    Avanzando con la navbar

--- my-app/src/components/navigation/Navbar.tsx
+++ my-app/src/components/navigation/Navbar.tsx
@@ -1,11 +1,22 @@
-import type { NextPage } from 'next'
+import { BsLinkedin, BsGithub, BsMailbox } from 'react-icons/Bs'
 
-export const Navbar: NextPage = () => {
+export const Navbar = () => {
   return (
-    <nav className='h-100 border-b-2'>
-      <a className="text-3xl font-bold">
+    <nav className='h-12 border-b-2 flex flex-row place-content-between items-center'>
+      <a className="text-2xl font-bold ml-8s">
         ../Portfolio/Juani
       </a>
+      <ul className='flex items-center w-24 place-content-around mr-8'>
+        <li className='w-3 h-3'>
+          <BsLinkedin />
+        </li>
+        <li>
+          <BsGithub />
+        </li>
+        <li>
+          <BsMailbox />
+        </li>
+      </ul>
     </nav>
     
   )
