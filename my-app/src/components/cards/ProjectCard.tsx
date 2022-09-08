468e380115fa7a92dcd11a3144e9a656063ed59d
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-09-08 12:28:49 -0300

    Logre darle el estilo que quiero a las cartas

--- my-app/src/components/cards/ProjectCard.tsx
+++ my-app/src/components/cards/ProjectCard.tsx
@@ -20,18 +20,20 @@ interface props {
     }
 }
 
+// box-shadow: 0_-6px_6px_0px_rgba(0,0,0,0)
+
 export const ProjectCard = ({project}: props) => {
     return (
-        <Card className="w-56 md:w-80 h-56 md:h-80 my-6 mx:3 md:m-6 p-2 md:p-3.5 flex justify-between rounded-lg border-2 border-black bg-white dark:bg-transparent dark:border-white">
-            <CardHeader color="blue" className="static h-20 md:h-28">
+        <Card className="w-56 md:w-80 h-56 md:h-80 my-6 mx:3 md:m-6 p-2 md:p-3.5 flex justify-between border-2 border-black bg-white dark:bg-transparent dark:border-white transition hover:shadow-[-6px_6px_0_0_rgba(0,0,0,1)] hover:translate-x-1 hover:-translate-y-1">
+            <CardHeader color="blue">
                 <img
                 src={project.image}
                 alt="img-blur-shadow"
-                className="absolute bottom-32 md:bottom-44 left-1.5 md:left-3.5 w-52 md:w-72 h-28 md:h-40 rounded-lg"
+                className="h-36 w-full object-cover"
                 />
             </CardHeader>
             <CardBody className="text-justify">
-                <Typography variant="h5" className="font-archivo font-semibold my-1 text-sm md:text-lg">
+                <Typography variant="h5" className="font-archivo font-semibold my-1 text-sm md:text-lg text-yellow-600">
                 {project.name}
                 </Typography>
                 <Typography variant='paragraph' className='font-archivo text-black dark:text-white font-medium my-1 text-xs md:text-sm h-12'>
@@ -42,6 +44,7 @@ export const ProjectCard = ({project}: props) => {
                 <ButtonRepo link={project.github}>REPO</ButtonRepo>
                 <ButtonDeploy link={project.deploy}>DEPLOY</ButtonDeploy>
             </CardFooter>
+            
         </Card>
     )
 }
