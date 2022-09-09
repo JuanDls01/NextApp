f43956b5a04e9613ae5f06dfb99a254c639b6676
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-09-09 16:27:42 -0300

    Agregando bg puntitos

--- my-app/src/pages/works.tsx
+++ my-app/src/pages/works.tsx
@@ -11,11 +11,11 @@ const Works: NextPage = () => {
       <div className='w-5/6 h-auto py-2 px-10 md:py-8 md:px-20 border-l-2 border-black dark:border-white'>
         <Title>Works</Title>
         <Paragragph>Repository of projects that I worked before</Paragragph>
-        <div className='flex flex-wrap justify-around'>
+        <div className="flex flex-wrap justify-around bg-[url('https://d39uo3k0liehyo.cloudfront.net/roadmap-v2/images/bg-pattern.png')] bg-transparent">
           {
             Projects && Projects.map(project => {
               return (
-                <ProjectCard project={project} />
+                <ProjectCard key={project.id} project={project} />
               )
             })
           }
