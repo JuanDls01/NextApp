4ff2a043c991adf089cd74e7230b3bbeafe13d69
JuanDls01 <juanignaciodelossantos01@gmail.com>
2022-09-10 18:42:57 -0300

    Agregando key a techlogos

--- my-app/src/components/TechLogos.tsx
+++ my-app/src/components/TechLogos.tsx
@@ -14,8 +14,8 @@ export const TechLogos = () => {
             {
                 technologies && technologies.map((technologie: technologie) => {
                     return(
-                        <div className='w-8 h-8 md:w-16 md:h-16 m-1 md:m-3 border-2 border-black rounded-sm flex items-center justify-center bg-white transition hover:shadow-[-3px_3px_0_0_rgba(0,0,0,1)] hover:translate-x-1 hover:-translate-y-1'>
-                            <Image key={technologie.id} src={technologie.image} className='object-scale-down scale-75 md:scale-100' width={30} height={30} />
+                        <div key={technologie.id} className='w-8 h-8 md:w-16 md:h-16 m-1 md:m-3 border-2 border-black rounded-sm flex items-center justify-center bg-white transition hover:shadow-[-3px_3px_0_0_rgba(0,0,0,1)] hover:translate-x-1 hover:-translate-y-1'>
+                            <Image src={technologie.image} className='object-scale-down scale-75 md:scale-100' width={30} height={30} />
                         </div>
                     )
                 })
