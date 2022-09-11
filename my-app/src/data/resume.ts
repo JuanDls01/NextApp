3f4a4db0ca7ce557ce9b51781e6386204320e5c7
Juan Ignacio <juanignaciodelossantos01@gmail.com>
2022-09-11 19:24:16 -0300

    Data Resume finished

--- my-app/src/data/resume.ts
+++ my-app/src/data/resume.ts
@@ -1,18 +1,26 @@
 export const resume = [
     {
-        category: 'Academic',
-        title: 'Ingeniería Industrial',
-        institución: 'Universidad Nacional del Litoral',
+        category: 'Professional Education',
+        title: 'Industrial Engineer',
+        institution: 'Universidad Nacional del Litoral',
         since: '01-01-2017',
         to: '',
-        description: 'Quinto año de Ingenería, 34/39 materias aprobadas. Buscando orientarme hacia la programación y el mundo de las tecnologías de la información'
+        description: 'Fifth year of Engineering, 34/39 approved subjects. Orientation towards programming and the world of information technologies'
     },
     {
-        type: 'Work',
+        category: 'Professional Experience',
         title: 'Intern Industrial Engineer',
         institution: 'Carlos Alberto García S.A.',
         since: '01-01-2021',
         to: '31-12-2021',
         description: 'Contacto con proveedores. Desarrollo de sistema de gestión de precios de productos de retail en Excel. Monitoreo y gestión de inventario y producción a granel',
-    }
+    },
+    {
+        category: 'Professional Education',
+        title: 'Full Stack Web Developer',
+        institution: 'Henry',
+        since: '15-01-2022',
+        to: '05-05-2022',
+        description: 'Bootcamp with +700 theoretical-practical hours. I learned the most demanded technologies like JavaScript, React, Redux, Node.JS, Express y PostgreSQL.'
+    },
 ]
--- my-app/src/pages/resume.tsx
+++ my-app/src/pages/resume.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import { Layout } from '../hocs/Layout';
 import { Title } from '../components/navigation/ui/Title';
 import { Paragragph } from '../components/navigation/ui/Paragraph';
+import { SubTitle } from '../components/navigation/ui/Subtitle';
 
 const Resume: NextPage = () => {
   return (
@@ -9,6 +10,8 @@ const Resume: NextPage = () => {
       <div className='w-5/6 h-auto py-2 px-5 sm:py-4 sm:px-10 md:py-8 md:px-20 border-l-2 border-black dark:border-white'>
         <Title>Resume</Title>
         <Paragragph>A summary of my education, work history, credentials, and other accomplishments and skills</Paragragph>
+        <SubTitle>Professional Experience</SubTitle>
+        <SubTitle>Professional Education</SubTitle>
       </div>
     </Layout>
   )
